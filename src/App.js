1b2fee3fa263368639db400ffe7ac911e18588a4
Merve Balci <mervebalci090@gmail.com>
2023-12-20 18:46:46 -0400

    Forming the app with adding components and CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import Wrapper from './components/Wrapper';
+import Screen from './components/Screen';
+import ButtonBox from './components/ButtonBox';
+import Button from './components/Button';
+
+const buttonValues = [
+  ["C", "+/-", "%", "÷"],
+  ["7", "8", "9", "x"],
+  ["4", "5", "6", "-"],
+  ["1", "2", "3", "+"],
+  ["0", ".", "="],
+]
 
 export default function App() {
   return (
     <div className="App">
-      App
+      <Wrapper>
+        <Screen />
+        <ButtonBox>
+          {buttonValues.flat().map((button, index) => {
+            return (
+              <Button
+                value={button}
+                key={index}
+              />
+            )
+          })}
+        </ButtonBox>
+      </Wrapper>
     </div>
   );
 }
