dbef31a5626974ad84d0545511e66891cc2fc308
Merve Balci <mervebalci090@gmail.com>
2023-12-25 18:08:09 -0400

    Handling Button Click

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,6 @@
+import { useContext } from "react"
+import { CalcContext } from "../context/CalcContext"
+
 function getStyleName(btn) {
   const className = {
     "=": "equals",
@@ -11,7 +14,99 @@ function getStyleName(btn) {
 }
 
 export default function Button({ value }) {
+  const { calc, setCalc } = useContext(CalcContext);
+
+  // When user clicks on decimal point button
+  function decimalClick() {
+    setCalc({...calc, number: !calc.number.toString().includes('.') ? calc.number + value : calc.number})
+  }
+
+  // When user clicks on clear button
+  function clearClick() {
+    setCalc({sign: '', number: 0, result: 0})
+  }
+
+  // When user clicks on the number 0-9 buttons
+  function handleNumberButton() {
+    const numberString = value.toString()
+
+    let numberValue;
+    if (numberString === '0' && calc.number === 0) {
+      numberValue = '0'
+    } else {
+      numberValue = Number(calc.number + numberString)
+    }
+
+    setCalc({...calc, number: numberValue})
+  }
+
+  // When user clicks on any operation buttons
+  function optClick() {
+    setCalc({
+      sign: value, 
+      result: !calc.result && calc.number ? calc.number : calc.result,
+      number: 0
+    })
+  }
+
+  // When user clicks on equals button
+  function equalsClick() {
+    if (calc.result && calc.number) {
+      function math(a, b, sign) {
+        const result = {
+          '+': (a, b) => a + b,
+          '-': (a, b) => a - b,
+          'x': (a, b) => a * b,
+          '÷': (a, b) => a / b,
+        }
+        return result[sign](a, b);
+      }
+      setCalc({
+        result: math(calc.result, calc.number, calc.sign),
+        sign: '',
+        number: 0,
+      })
+    }
+  }
+
+  // When user clicks on percent button
+  function percentClick() {
+    setCalc({
+      number: (calc.number / 100),
+      result: (calc.result / 100),
+      sign: '',
+    })
+  }
+
+  // When user clicks on "+/-" button
+  function invertClick() {
+    setCalc({
+      number: calc.number ? calc.number * -1 : 0,
+      result: calc.result ? calc.result * -1 : 0,
+      sign: '',
+    })
+  }
+
+  function handleBtnClick() {
+    const results = {
+      '.': decimalClick,
+      'C': clearClick,
+      '+': optClick,
+      '-': optClick,
+      'x': optClick,
+      '÷': optClick,
+      '=': equalsClick,
+      '%': percentClick,
+      '+/-': invertClick,
+    }
+    if (results[value]) {
+      return results[value]()
+    } else {
+        return handleNumberButton()
+    }
+  }
+
   return (
-    <button className={`${getStyleName(value)} button`}>{value}</button>
+    <button onClick={handleBtnClick} className={`${getStyleName(value)} button`}>{value}</button>
   )
 }
