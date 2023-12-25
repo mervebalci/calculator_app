87fd6428d52ddabe88bf00c04a4ad3f410f51585
Merve Balci <mervebalci090@gmail.com>
2023-12-25 16:43:17 -0400

    Creating Context API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Wrapper from './components/Wrapper';
 import Screen from './components/Screen';
 import ButtonBox from './components/ButtonBox';
 import Button from './components/Button';
+import CalcProvider from './context/CalcContext';
 
 const buttonValues = [
   ["C", "+/-", "%", "÷"],
@@ -15,19 +16,21 @@ const buttonValues = [
 export default function App() {
   return (
     <div className="App">
-      <Wrapper>
-        <Screen />
-        <ButtonBox>
-          {buttonValues.flat().map((button, index) => {
-            return (
-              <Button
-                value={button}
-                key={index}
-              />
-            )
-          })}
-        </ButtonBox>
-      </Wrapper>
+      <CalcProvider>
+        <Wrapper>
+          <Screen />
+          <ButtonBox>
+            {buttonValues.flat().map((button, index) => {
+              return (
+                <Button
+                  value={button}
+                  key={index}
+                />
+              )
+            })}
+          </ButtonBox>
+        </Wrapper>
+      </CalcProvider>
     </div>
   );
 }
